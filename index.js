44946e6ee9a934b1faa6470caa9c8274bb5be20c
Rajiv Kashyap Jalakam (Student) <rjalakam@sundevils.asu.edu>
2023-04-22 11:00:44 -0700

    Updates

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ app.use(express.json());
 app.listen(3000, () => {
     console.log('Server Started at ${3000}')
 })
+
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error);
+})
+
+database.once('connected', () => {
+    console.log("Successful");
+})
+
+const routes = require('./routes/routes');
+app.use('/api', routes)
